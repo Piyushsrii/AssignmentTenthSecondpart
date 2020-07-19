05a1e23aa332de46f204b71d2b4d8c98431b20b1
piyushsrii <piyushnamo888@gmail.com>
2020-07-20 00:32:55 +0530

    commit in uc2

--- AssignmentSencond.sh
+++ AssignmentSencond.sh
@@ -1,1 +1,18 @@
 Welcome
+
+#!/bin/bash -x
+
+win=0;
+bats=0
+for((i=1;i<=10;i++))
+do
+randomvar=$((RANDOM%2))
+if [[ $randomvar == 1 ]]
+then
+((win++))
+echo "Head $win"
+else
+((bats++))
+echo "Tail $bats"
+fi
+done
