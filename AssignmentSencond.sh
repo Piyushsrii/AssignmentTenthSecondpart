506aadf79823331aef187e69148457e33624abac
piyushsrii <piyushnamo888@gmail.com>
2020-07-20 00:45:05 +0530

    commit in uc3

--- AssignmentSencond.sh
+++ AssignmentSencond.sh
@@ -1,1 +1,23 @@
 Welcome
+
+#!/bin/bash -x       #UserCase----->3
+
+HeadWins=0;
+TailWins=0
+for((i=1;i<=42;i++))
+do
+randomvar=$((RANDOM%2))
+if [[ $randomvar == 1 ]]
+then
+if [[ $HeadWins -le 21 ]]
+then
+((HeadWins++))
+echo $HeadWins
+fi
+else
+((TailWins++))
+echo $TailWins
+fi
+done
+echo "if Head come greater than 21 than head win" $HeadWins
+echo "if tail come greater than 21 than Tail win"$TailWins
